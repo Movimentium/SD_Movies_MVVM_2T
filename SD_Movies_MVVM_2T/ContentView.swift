1e7db8deb75232f455bcb3827626f4b607ba883e
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-09 13:34:04 +0100

    Initial Commit

---
+++ SD_Movies_MVVM_2T/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SD_Movies_MVVM_2T
+//
+//  Created by Miguel Gallego on 9/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SD_Movies_MVVM_2T/SD_Movies_MVVM_2TApp.swift
@@ -0,0 +1,17 @@
+//
+//  SD_Movies_MVVM_2TApp.swift
+//  SD_Movies_MVVM_2T
+//
+//  Created by Miguel Gallego on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SD_Movies_MVVM_2TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
